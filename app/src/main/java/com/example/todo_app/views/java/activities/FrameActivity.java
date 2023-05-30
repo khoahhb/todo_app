5d3656c613446de6ede2c59b37df6c02fda99d99
khoahhb <khoahhb@gmail.com>
2023-05-30 22:23:02 +0700

    Add backward to java

--- app/src/main/java/com/example/todo_app/views/java/activities/FrameActivity.java
+++ app/src/main/java/com/example/todo_app/views/java/activities/FrameActivity.java
@@ -1,6 +1,10 @@
 package com.example.todo_app.views.java.activities;
 
+import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
@@ -11,5 +15,16 @@ public class FrameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.frame);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        setupActionBarWithNavController(this, navController);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.frame);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
